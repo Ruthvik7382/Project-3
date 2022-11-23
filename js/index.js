d9cc31f3558e962485e52d93a3187b510c639f4b
RUTHVIK KILARU <74092141+Ruthvik7382@users.noreply.github.com>
2022-11-23 01:10:42 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
-    center: { lat: -37.819954, lng: 144.983398},
+    center: { lat: 16.648169, lng: 80.615398},
     gestureHandling: "none",
     zoomControl: false,
     scaleControl: true,
   });
   
   var marker = new google.maps.Marker({
-    position: { lat: -37.819954, lng: 144.983398 },
+    position: { lat: 16.648169, lng: 80.615398 },
     map: map,
   });
   
   var infowindow = new google.maps.InfoWindow({
-    content:"My Favourite Cricket Ground"
+    content:"Village Home"
   });
   infowindow.open(map, marker);
 }
